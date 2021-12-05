5bbb5a4c64d711f50fde2dba8e6c01126a94096b
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2021-12-05 14:17:22 -0300

    UPDATE forms da senha

--- Js/senhas.js
+++ Js/senhas.js
@@ -1,11 +1,37 @@
-const form = document.getElementById('opcoes-senha');
-const btn =  document.getElementById('btn-opcoes');
-const box = document.getElementById('box-forms');
+const op1 = document.getElementById('senhaChoice1');
+const op2 = document.getElementById('senhaChoice2');
+const op3 = document.getElementById('senhaChoice3');
+const op4 = document.getElementById('senhaChoice4');
 
+var f1 = document.getElementById("f1");
+var f2 = document.getElementById("f2");
+var f3 = document.getElementById("f3");
+var f4 = document.getElementById("f4");
 
-btn.addEventListener('click', function(event) {
-  event.preventDefault();
-  let divQueVaiAparecer = document.getElementById(form.choice.value);
-  divQueVaiAparecer.classList.toggle("invisivel");
-  console.log(divQueVaiAparecer);
+op1.addEventListener('click', function() {
+  f1.classList.remove('disabled');
+  f2.classList.add('disabled');
+  f3.classList.add('disabled');
+  f4.classList.add('disabled');
 });
+
+op2.addEventListener('click', function() {
+  f2.classList.remove('disabled');
+  f1.classList.add('disabled');
+  f3.classList.add('disabled');
+  f4.classList.add('disabled');
+});
+
+op3.addEventListener('click', function() {
+  f3.classList.remove('disabled');
+  f2.classList.add('disabled');
+  f1.classList.add('disabled');
+  f4.classList.add('disabled');
+})
+
+op4.addEventListener('click', function() {
+  f4.classList.remove('disabled');
+  f2.classList.add('disabled');
+  f1.classList.add('disabled');
+  f3.classList.add('disabled');
+})
